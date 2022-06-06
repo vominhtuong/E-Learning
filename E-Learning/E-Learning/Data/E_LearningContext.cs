f03c16c2a4b19f15f5f13ec9a68d00a6649ed01e
vominhtuong <vominhtuong10022000@gmail.com>
2022-06-06 09:36:43 +0700

    up

---
+++ E-Learning/E-Learning/Data/E_LearningContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using E_Learning.Models;
+
+namespace E_Learning.Data
+{
+    public class E_LearningContext : DbContext
+    {
+        public E_LearningContext (DbContextOptions<E_LearningContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<E_Learning.Models.User>? User { get; set; }
+
+        public DbSet<E_Learning.Models.Student>? Student { get; set; }
+    }
+}
---
+++ E-Learning/E-Learning/Migrations/20220606020716_IntialData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Learning.Migrations
+{
+    public partial class IntialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentCode = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Grade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
