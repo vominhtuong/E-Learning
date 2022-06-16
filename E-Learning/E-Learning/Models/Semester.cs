c84a508a79ad70e19e5df3e483abfa38eb9124e1
vominhtuong <vominhtuong10022000@gmail.com>
2022-06-16 11:07:21 +0700

    up

--- E-Learning/E-Learning/Models/Semester.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace E_Learning.Models
-{
-    public class Semester
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Class> Classes { get; set; }
-    }
-}
